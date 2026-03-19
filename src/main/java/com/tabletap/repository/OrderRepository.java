b6ed21e730cea3490d8ea94851cfa3bff27400cc
Anto <antomanjooran@gmail.com>
2026-03-19 19:46:48 +0000

    fix Order Repo

--- src/main/java/com/tabletap/repository/OrderRepository.java
+++ src/main/java/com/tabletap/repository/OrderRepository.java
@@ -1,11 +1,15 @@
-=package com.tabletap.repository;
+package com.tabletap.repository;
+
 import com.tabletap.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.time.Instant;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
